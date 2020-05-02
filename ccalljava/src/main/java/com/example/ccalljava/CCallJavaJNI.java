79e71377143bea24b00e357bf0d41f2b3d7b9d0b
justinworkshop <justinworkshop@163.com>
2020-05-02 16:25:18 +0800

    add c call java

---
+++ ccalljava/src/main/java/com/example/ccalljava/CCallJavaJNI.java
@@ -0,0 +1,24 @@
+package com.example.ccalljava;
+
+/**
+ * Copyright (C), 2016-2020
+ * FileName: CCallJavaJNI
+ * Author: zhengwei
+ * Date: 2020-05-02 15:12
+ * Description: C call Java
+ */
+public class CCallJavaJNI {
+    public native void callbackAdd();
+
+    public native void callbackGetMessage();
+
+    public int add(int x, int y) {
+        System.out.println("Invoke add: x= " + x + ", y= " + y);
+        return x + y;
+    }
+
+    public void getMessage(String s) {
+        System.out.println("Invoke getMessage: s= " + s);
+    }
+
+}
--- ccalljava/src/main/java/com/example/ccalljava/CCallJava_MainActivity.java
+++ ccalljava/src/main/java/com/example/ccalljava/CCallJava_MainActivity.java
@@ -6,9 +6,17 @@ import android.os.Bundle;
 
 public class CCallJava_MainActivity extends AppCompatActivity {
 
+    {
+        System.loadLibrary("c_call_java");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_c_call_java_main);
+
+        CCallJavaJNI cCallJavaJNI = new CCallJavaJNI();
+        cCallJavaJNI.callbackAdd();
+        cCallJavaJNI.callbackGetMessage();
     }
 }
